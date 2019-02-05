e4d320fa5ad900afdb648f1847baa5751f8c321d
wensjuma <wensjuma@gmail.com>
2019-02-06 01:14:01 +0300

    [Finishes #163711740] Retrieve Political Party List

--- app/api/v1/views/party_view.py
+++ app/api/v1/views/party_view.py
@@ -4,7 +4,7 @@ from app.api.v1.models import party_model
 PARTY = party_model.Party()
 
 party_route = Blueprint('party', __name__, url_prefix='/api/v1')
-@party_route.route('/',methods=['GET'])
+@party_route.route('/getparties',methods=['GET'])
 def get_parties():
     data = PARTY.get_parties()
     return make_response(jsonify({
