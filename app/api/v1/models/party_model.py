56ab0b9599687ccf2e373cc6131f4503deb6bafb
wensjuma <wensjuma@gmail.com>
2019-02-06 00:42:57 +0300

    [Finishes #163712011] Create party

---
+++ app/api/v1/models/party_model.py
@@ -0,0 +1,19 @@
+
+PARTY=[]
+
+class Party(object):
+    def __init__(self):
+        self.party = PARTY
+    
+    def get_parties(self):
+        return self.party
+    
+    def add_party(self, name, slogan):
+        data = {
+            'id':len(self.party)+1,
+            'title':name,
+            'slogan':slogan
+        }
+        self.party.append(data)
+        return self.party
+    
---
+++ app/api/v1/views/party_view.py
@@ -0,0 +1,24 @@
+from flask import Flask, Blueprint, make_response, request,jsonify 
+from app.api.v1.models import party_model
+
+PARTY = party_model.Party()
+
+party_route = Blueprint('party', __name__, url_prefix='/api/v1')
+@party_route.route('/',methods=['GET'])
+def get_parties():
+    data = PARTY.get_parties()
+    return make_response(jsonify({
+        'status':200,
+        'data':data
+    })),200
+@party_route.route('/addparty', methods=['POST'])
+def save_party():
+        data= request.get_json()
+        name= data['name']
+        slogan= data['slogan']
+        
+        PARTY.add_party(name, slogan)
+        return make_response(jsonify({
+            "status":201,
+            "data":"Party Added successifuly!"
+        }),201)
