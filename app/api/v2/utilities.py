0754a163927a95b84bf1b7d103a2f7bf59e19308
wensjuma <wensjuma@gmail.com>
2019-02-14 01:55:43 +0300

    [Finishes #163923445] Create account login endpoint

---
+++ app/api/v2/utilities.py
@@ -0,0 +1,85 @@
+import os
+import re
+from flask import abort, request
+from functools import wraps
+from app.api.utils import res_method
+from app.api.v2.models.database import select_data_from_db
+from flask_jwt import jwt
+from flask_jwt import JWT
+
+KEY = os.getenv('SECRET_KEY')
+
+"""
+consists of utilities required by 
+different files in v2 of this app
+"""
+
+
+def PasswordsMatch(first_pass, sec_pass):
+    """
+        this function checks if the passwords.
+    """
+    if(first_pass!= sec_pass):
+        abort(res_method(400, "error", "passwords dont match"))
+    return True
+
+
+def is_phone_number_valid(phone):
+    """
+        This checks if a number phone number is valid
+    """
+    if not re.match('^[0-9]*$', phone):
+        abort(res_method(400, "Error", "Phone number should be integers only"))
+
+
+def isEmailValid(email):
+    """
+        this function checks if the email is valid
+        via regex
+    """
+    if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", email):
+        abort(res_method(400, "error", "email is invalid"))
+    return True
+
+
+def check_matching_items_in_db_table(params, table_name):
+    """
+        check if a value of key provided is 
+        available in the database table
+        if there's a duplicate then the test fails
+    """
+    for key, value in params.items():
+        query = """
+        SELECT {} from {} WHERE {}.{} = '{}'
+        """.format(key, table_name, table_name, key, value)
+        duplicated = select_data_from_db(query)
+        if duplicated:
+            abort(res_method(409, "error",
+                              "Error. '{}' '{}' is already in use".format(key, value)))
+
+
+def token_required(f):
+    """
+        This higher order function checks for token in the request
+        Headers
+    """
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return res_method(401, "error", "Token is missing")
+        try:
+            data = jwt.decode(token, KEY, algorithms='HS256')
+            query = """
+            SELECT email FROM users
+            WHERE users.email = '{}'""".format(data['email'])
+
+            user = select_data_from_db(query)
+
+        except:
+            return res_method(401, "error", "Token is expired or invalid")
+
+        return f(user, *args, **kwargs)
+    return decorated
---
+++ app/api/v2/views/users.py
@@ -0,0 +1,126 @@
+from flask import request, abort, Blueprint, make_response, jsonify, request
+import datetime
+from app.api import utils
+from app.api.v2 import utilities
+from app.api.v2.models.model_users import UserModel
+import psycopg2
+
+import os
+import jwt
+user_blueprints= Blueprint('users', __name__, url_prefix="/api/v2")
+
+KEY = os.getenv('SECRET_KEY')
+
+
+@user_blueprints.route("/auth/signup", methods=["POST"])
+def signup():
+    """
+        Sign a user up
+    """
+    try:
+        data = request.get_json()
+        firstname = data['firstname']
+        lastname = data['lastname']
+        username = data["username"]
+        othername = data.get("othername", "none")
+        email = data["email"]
+        phone = data["phone"]
+        passportUrl = data.get("passportUrl")
+        password = data["password"]
+        retypedpassword = data["retypedpassword"]
+        isPolitician = data.get("isPolitician", False)
+
+    except:
+       return abort(utils.res_method(400, "error", 'wrong json formated keys'))
+    
+    utilities.PasswordsMatch(password, retypedpassword)
+    
+    utilities.isEmailValid(email)
+    utilities.is_phone_number_valid(phone)
+
+    utilities.check_matching_items_in_db_table({"username": username}, "users")
+    utilities.check_matching_items_in_db_table({"email": email}, "users")
+
+    newuser = UserModel(username, email, password, firstname,
+                        lastname, phone, passportUrl, isPolitician, othername)
+    newuser.save_user()
+
+    return utils.res_method(201, "data", [{
+        "user": {
+            "email": newuser.email,
+            "username": newuser.username
+        },
+        "token": newuser.password
+    }])
+
+
+# login route
+@user_blueprints.route("/auth/signin", methods=['POST'])
+def user_login():
+    try:
+        data = request.get_json()
+        request_email = data['email']
+        password = data['password']
+
+    except KeyError:
+        abort(utils.res_method(400, "error", "Provide correct values for email & password"))
+
+    utilities.isEmailValid(request_email)
+    try:
+        user = UserModel.get_user_by_mail(request_email)
+        if not user:
+            abort(utils.res_method(404, "error", "User does not exist"))
+
+
+        # id = user[0][0]
+        # username = user[0][1]
+        hashed_password = user[0][2]
+        req_email= request_email.strip()
+
+        password = UserModel.check_if_password_n_hash_match(
+            hashed_password, password)
+        if not password:
+            return make_response(jsonify({
+            "message": "Try again. E-mail or password is incorrect!"
+        }
+        ), 403)
+
+        token = jwt.encode({
+                "email": req_email,
+                # "user_id": user[0]['user_id'],
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=3000)
+            }, os.getenv('JWT_SECRET_KEY', default='SdaHv342nx!jknr837bjwd?c,lsajjjhw673hdsbgeh'))
+        return make_response(jsonify({
+                            "message": "Login successful",
+                            "token": token.decode("UTF-8"),
+                            # "role": user[0]['role']
+                            }), 200)
+
+        
+    except psycopg2.DatabaseError as _error:
+        abort(utils.res_method(500, "error", "Server error"))
+
+
+# password reset route
+@user_blueprints.route("/auth/reset", methods=["POST"])
+def reset_password():
+    try:
+        data = request.get_json()
+        email = data["email"]
+    except KeyError:
+        abort(utils.res_method(400, "error", "Should be email"))
+
+    
+    utilities.isEmailValid(email)
+    
+    try:
+        user = UserModel.get_user_by_mail(email)
+        if not user:
+            abort(utils.res_method(404, "error",
+                                    "User does not exist. Create an account first"))
+        return utils.res_method(200, "data", [{
+            "message": "Check your email for password reset link",
+            "email": email
+        }])
+    except psycopg2.DatabaseError as _error:
+        abort(utils.res_method(500, "error", "Server error"))
