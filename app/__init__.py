bdd808039b5a582f94c63f052f3905466483703a
wensjuma <wensjuma@gmail.com>
2019-02-14 01:18:00 +0300

    [Starts #163924584] Implement JWT tocken based auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,24 @@
 import os 
 from flask import Flask
-from app.api.v1.views import party_view
-from app.api.v1.views import offices
 from config import app_config
+from app.api.v2.models.database import init_db
 
 def create_app(app_config):
     app= Flask(__name__)
     app.config.from_object([app_config])
+    init_db
+
+    from app.api.v1.views import party_view
     app.register_blueprint(party_view.party_route)
+
+    from app.api.v1.views import offices
     app.register_blueprint(offices.office_route)
 
+    from app.api.v2.views.parties import party_blueprints
+    app.register_blueprint(party_blueprints)
+
+    from app.api.v2.views.users import user_blueprints
+    app.register_blueprint(user_blueprints)
+    
+    
     return app
